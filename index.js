23089879d0ae3519b95138dd77c2579efb7dc256
talati09 <stalati04@gmail.com.com>
2026-02-17 01:30:47 +0530

    express

---
+++ index.js
@@ -0,0 +1,35 @@
+const express=require('express')
+
+const users=require('./MOCK_DATA.json');
+
+const app=express();
+const port=8080;
+
+
+app.get("/users",(req,res)=>{
+     const html=`
+       
+        <ul>    
+            ${users.map(user=>`<li>${user.first_name} <\li>`)}
+
+        </ul>
+
+     `;
+      res.send(html);
+});
+//routes
+app.get("/api/users",(req,res)=>{
+     return res.json(users)
+})
+//dynamic path
+app.get("/api/users/:id",(req,res)=>{
+    const id=Number(req.params.id);
+
+    const user=users.find((user)=>user.id===id);
+    return res.json(user);
+
+})
+
+
+
+app.listen(port,()=>console.log("server started at port 8080"));
